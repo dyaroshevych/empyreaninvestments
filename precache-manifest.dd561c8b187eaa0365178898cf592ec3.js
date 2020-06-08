98fc798809bbac77a60d1be2a4af3a391f662841
dyaroshevych <dyaroshevych@gmail.com>
2020-06-09 00:36:41 +0300

    Updates

---
+++ precache-manifest.dd561c8b187eaa0365178898cf592ec3.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee0805b85b21e100a2b45790d9800683",
+    "url": "/empyreaninvestments/index.html"
+  },
+  {
+    "revision": "3c8fe6c0b7ee16142f10",
+    "url": "/empyreaninvestments/static/css/main.a9477927.chunk.css"
+  },
+  {
+    "revision": "c6ff0b51aa36facde9ca",
+    "url": "/empyreaninvestments/static/js/2.655b047f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/empyreaninvestments/static/js/2.655b047f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3c8fe6c0b7ee16142f10",
+    "url": "/empyreaninvestments/static/js/main.1cea2f31.chunk.js"
+  },
+  {
+    "revision": "b2bc5e7040dac9528b9d",
+    "url": "/empyreaninvestments/static/js/runtime-main.1fc57353.js"
+  },
+  {
+    "revision": "476c6235fdf8832eb1047307665426ae",
+    "url": "/empyreaninvestments/static/media/header-bg.476c6235.jpg"
+  },
+  {
+    "revision": "ed2a5139401e7c4498dec0af58f862ef",
+    "url": "/empyreaninvestments/static/media/investment-bg.ed2a5139.webp"
+  },
+  {
+    "revision": "30a3f003116cd6f57d4f1101b86139f6",
+    "url": "/empyreaninvestments/static/media/link-strategy.30a3f003.jpg"
+  },
+  {
+    "revision": "7ee73e5e212d71fea73301dcf7abf0c3",
+    "url": "/empyreaninvestments/static/media/link-team.7ee73e5e.jpg"
+  },
+  {
+    "revision": "6719431d6075f3639ff5bd84a5a34b79",
+    "url": "/empyreaninvestments/static/media/location.6719431d.svg"
+  },
+  {
+    "revision": "48604ea36756d6038837f1528ace9a3a",
+    "url": "/empyreaninvestments/static/media/logo.48604ea3.webp"
+  },
+  {
+    "revision": "bfe8a6b50ba5128064b2294299fa8cc4",
+    "url": "/empyreaninvestments/static/media/mail.bfe8a6b5.svg"
+  },
+  {
+    "revision": "578398cee8105e54986283266ceee98a",
+    "url": "/empyreaninvestments/static/media/phone.578398ce.svg"
+  }
+]);
